f1c567a3eb9a1a39f1b3b78f9a2dbc529d365656
codeme254 <otwomadennis365@gmail.com>
2022-08-20 13:26:37 +0300

    just smart formatting

--- src/Skills/Skill.jsx
+++ src/Skills/Skill.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 // import testImg from '../Assets/Images/static-web-app.svg';
 
-const Skill = ({skillName, tools, skillImage}) => {
-    return(
-        <div className="skill">
-            <img src={skillImage} alt={skillName} className="skill__image" />
-            <p className="skill__skill-name">{skillName}</p>
-            <p className="skill__tools">{tools}</p>
-        </div>
-    )
-}
+const Skill = ({ skillName, tools, skillImage }) => {
+  return (
+    <div className="skill">
+      <img src={skillImage} alt={skillName} className="skill__image" />
+      <p className="skill__skill-name">{skillName}</p>
+      <p className="skill__tools">{tools}</p>
+    </div>
+  );
+};
 
 export default Skill;
